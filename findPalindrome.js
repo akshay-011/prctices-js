6f7984addaaa3279a3c14bd8a884712c1c94ce1a
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2024-12-21 16:02:36 +0530

    removed palindrome

--- findPalindrome.js
+++
@@ -1,3 +0,0 @@
-// Write a function isPalindrome(str) that checks whether a string is a palindrome. 
-// A palindrome is a word, phrase, or sequence of characters that reads the same
-// backward as forward (ignoring spaces and punctuation).
