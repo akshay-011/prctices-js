39decb5e892f9f7fc8ddb3a9bcc508fcf78793dd
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2024-12-21 14:37:55 +0530

    Added some testcases and passed

--- flattenArray.js
+++ flattenArray.js
@@ -3,3 +3,55 @@
 
 // Example:
 // flattenArray([1, [2, 3], [4, [5, 6]]]) → [1, 2, 3, 4, 5, 6]
+
+const flattenArray = function (nestedArray) {
+  if (nestedArray.length === 0) {
+    return nestedArray;
+  }
+
+};
+
+// ======================>Testing starts<=================================
+const areArraysEqual = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let index = 0; index < array1.length; index++) {
+    if (!isSame(array1[index], array2[index])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isSame = function (value1, value2) {
+  if (Array.isArray(value1)) {
+    return areArraysEqual(value1, value2);
+  }
+
+  return value1 === value2;
+};
+
+const testCases = [
+  [flattenArray, [[]], []],
+  [flattenArray, [[1, 2, [1]]], [1, 2, 1]],
+];
+
+const test = function (failed, [fname, inputs, expcted]) {
+  const actual = fname(...inputs);
+
+  if (!isSame(actual, expcted)) {
+    failed.push([fname, inputs, expcted, actual]);
+  }
+
+  return failed;
+};
+
+const testAll = function (testCases) {
+  const failed = testCases.reduce(test, []);
+  console.table(failed);
+};
+
+testAll(testCases);
