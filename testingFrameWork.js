6fe2d7c8d23f2b142d7f749d8bd167a08a758a3a
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2024-12-27 23:52:18 +0530

    added objectfor equality check

--- testingFrameWork.js
+++ testingFrameWork.js
@@ -1,21 +1,29 @@
-// ======================>Testing starts<=================================
-const areArraysEqual = function (value1, value2) {
-  if (value1.length !== value2.length) {
+const areArraysEqual = function (array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   }
 
-  for (let index = 0; index < value1.length; index++) {
-    if (!isSame(value1[index], value2[index])) {
-      return false;
-    }
-  }
-
-  return true;
+  return array1.every((element, index) => isSame(element, array2[index]));
 };
 
-const isSame = function (value1, value2) {
+const areObjectsSame = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1).sort();
+  const obj2Keys = Object.keys(obj2).sort();
+
+  if (!areArraysEqual(obj1Keys, obj2Keys)) {
+    return false;
+  }
+
+  return obj1Keys.every((key) => isSame(obj1[key], obj2[key]));
+};
+
+export const isSame = function (value1, value2) {
   if (Array.isArray(value1)) {
     return areArraysEqual(value1, value2);
+  }
+
+  if (typeof value1 === "object") {
+    return areObjectsSame(value1, value2);
   }
 
   return value1 === value2;
